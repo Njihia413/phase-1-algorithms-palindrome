a5207aec0d1e4ed5da79643aeda21c2d453c9b5c
Maureen Njihia <maureennjihia468@gmail.com>
2022-08-25 17:28:53 +0300

    commit tree 20e18d01c5f2638bb654a6bb1fd4d843b6d508bb

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length;
+  for(let i = 0; i < len / 2; i++) {
+    if(word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+  1. Initialize a variable called len that checks the string length
+
+  2. Loop through half of the sring
+
+  3. Check if first and last string are the same
+  
+  4. If they are, return "String is a palindrome"
+
+  5. Else return "String is not a palindrome"
+
 */
 
 /*
